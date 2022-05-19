f4a0f5872c90518ddee19ac273d665285e05d9df
uf-code <ugur.firat.developer@gmail.com>
2022-05-19 21:58:00 +0300

    intro components, props & state

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
 
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
     return (
         <div className='container'>
             <h1>My App</h1>
-            <p>Hello</p>
         </div>
     )
 }
