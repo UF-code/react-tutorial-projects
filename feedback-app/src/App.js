19be4927fbbf0486f4f8c7754eaf8d8e169ad2b6
uf-code <ugur.firat.developer@gmail.com>
2022-05-19 22:39:42 +0300

    adding styles component

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+// Components
+import Header from './components/Header'
 
 function App() {
     return (
-        <div className='container'>
-            <h1>My App</h1>
-        </div>
+        <>
+            <Header bgColor='red' textColor='blue' text={'Feedback UI'} />
+            <div className='container'>
+                <h1>My App</h1>
+            </div>
+        </>
     )
 }
 
