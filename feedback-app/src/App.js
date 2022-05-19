6b960680dcb5e8f7f2c74ed25dbb20130764a140
uf-code <ugur.firat.developer@gmail.com>
2022-05-19 21:41:31 +0300

    intro to jsx

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,4 +1,8 @@
+import React from 'react'
+
 function App() {
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
     return (
         <div className='container'>
             <h1>My App</h1>
