92a361ade71f776a1422d8fd9521befe2fa8369a
uf-code <ugur.firat.developer@gmail.com>
2022-05-19 22:45:11 +0300

    using default props

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header'
 function App() {
     return (
         <>
-            <Header bgColor='red' textColor='blue' text={'Feedback UI'} />
+            <Header />
             <div className='container'>
                 <h1>My App</h1>
             </div>
